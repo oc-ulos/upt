5609f9a8a798385a19d94547ee6f0411944bf82f
Ocawesome101 <ocawesome101@gmail.com>
2022-10-19 20:43:48 -0400

    finish (?) libmtar, i think

--- src/lib/libmtar.lua
+++ src/lib/libmtar.lua
@@ -2,11 +2,12 @@
 -- @module libmtar
 -- @alias lib
 
-
 -- only supports mtar v2 files
 -- TODO: maybe move to its own package?
 
 local lib = {}
+local checkArg = require("checkArg")
+local stat = require("posix.sys.stat")
 
 local HEADER = "\xFF\xFF\x02M"
 
@@ -17,18 +18,24 @@ end
 local function readFileTags(handle, tagsize)
   local have_read = 0
   local tags = {}
+
   while have_read < tagsize do
     local tagspec_length = readint(handle, 2)
     local tag_name_length = readint(handle, 1)
     local tag_name = handle:read(tag_name_length)
     local tag_data_length = readint(handle, 2)
     local tag_data = handle:read(tag_data_length)
+
     if tagspec_length ~= tag_name_length + tag_data_length + 5 then
       error(string.format("mismatched tagspec length (specified %d, got %d)",
         tagspec_length, tag_name_length + tag_data_length + 5))
     end
+
     have_read = have_read + tagspec_length
+    tags[tag_name] = tag_data
   end
+
+  return tags
 end
 
 local function readFileRecord(handle)
@@ -37,7 +44,7 @@ local function readFileRecord(handle)
   local name = handle:read(name_length)
   local tagsize = readint(handle, 2)
   local tags = readFileTags(handle, tagsize)
-  local datsize = readint(handle, 8)
+  local datasize = readint(handle, 8)
   return recsize, name, tags, datasize
 end
 
@@ -63,18 +70,103 @@ end
 -- @type Writer
 local writer = {}
 
+local function bytes(n)
+  local b = 0
+  while n > 0 do n = n << 8 b = b + 1 end
+  return b
+end
+
+local function verifyRecord(r)
+  checkArg(1, r, "table")
+  checkArg("name", r.name, "string")
+  checkArg("tags", r.tags, "table")
+  for k, v in pairs(r.tags) do
+    checkArg("tags[?]", k, "string")
+    checkArg("tags["..k.."]", v, "string", "number")
+    if type(v) == "number" then r.tags[k] = string.pack("<I"..bytes(v), v) end
+  end
+end
+
 --- add a file from disk to the output
-function writer:add()
+-- throws on invalid file
+-- streams data in chunks so as to not over-fill RAM
+---@tparam string file The file to add
+---@tparam string path The path to which that file will be extracted
+function writer:add(file, path)
+  checkArg(1, file, "string")
+  checkArg(2, path, "string")
+
+  local statx, err = stat.stat(file)
+  if not statx then
+    error("add " .. file .. ": " .. err)
+  end
+
+  local record = {
+    name = path,
+    tags = {
+      mtime = statx.st_mtime,
+      mode = statx.st_mode,
+    },
+  }
+
+  self:create(record)
+
+  local handle, oerr = io.open(file, "r")
+  if not handle then
+    error("add " .. file .. ": " .. oerr)
+  end
+
+  repeat
+    local data = handle:read(statx.st_blksize)
+    if data then self.stream:write(data) end
+  until not data
+
+  handle:close()
+
+  return true
+end
+
+local function countBytes(number)
+  for i=0, 32 do
+    if 2^i > number then
+      return i
+    end
+  end
+
+  error("number exceeded 32 bytes in size - this situation should never occur")
 end
 
 --- add a custom file to the output
-function writer:create()
+function writer:create(record, data)
+  verifyRecord(record)
+
+  local tags = ""
+  for k, v in pairs(record.tags) do
+    if type(v) == "number" then
+      v = string.pack("<I"..countBytes(v), v)
+    end
+
+    local tag = string.pack("<s1s2", k, v)
+    tags = tags .. string.pack("<I2", #tag) .. tag
+  end
+
+  self.stream:write(string.pack("<I8s1s2I8",
+    8 + 1 + #record.name + 2 + #tags + 8 + #data,
+    record.name, tags, #data))
+  self.stream:write(data)
+
+  return true
+end
+
+function writer:close()
+  pcall(self.stream.close, self.stream)
 end
 
 --- Write MTAR data to a stream.
 -- This function expects 
 function lib.writeto(stream)
-  
+  stream:write(HEADER)
+  return setmetatable({stream=stream}, {__index=writer})
 end
 
 return lib
