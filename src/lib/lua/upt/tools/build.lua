f8f96a285ff839ed4df05c93cd4f3090d8e9d23b
Ocawesome101 <ocawesome101@gmail.com>
2023-09-17 22:52:37 -0400

    small theoretical fix for incorrect package sizes

--- src/lib/lua/upt/tools/build.lua
+++ src/lib/lua/upt/tools/build.lua
@@ -132,7 +132,7 @@ function lib.build(options)
     end
   end
 
-  local size = out:seek("cur")
+  local size = out:seek("cur")+130
   local mdata = meta.assemble(--string.format("%s %s %d:%s:%s:%s:%s",
     options.name, options.version, size, options.authors,
     options.depends or "", options.license or "", options.description)
